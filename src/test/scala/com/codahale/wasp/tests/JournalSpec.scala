f033615889a4bc62f60fa96fcc3ea735d890de26
Coda Hale <coda.hale@gmail.com>
2011-10-07 14:22:03 -0700

    Upgraded dependencies.
    Now on Scala 2.9.1.

--- src/test/scala/com/codahale/wasp/tests/JournalSpec.scala
+++ src/test/scala/com/codahale/wasp/tests/JournalSpec.scala
@@ -1,75 +1,24 @@
 package com.codahale.wasp.tests
 
-import com.codahale.wasp.Journal
 import java.io.File
-import java.util.concurrent.atomic.AtomicInteger
 import scala.collection.immutable.SortedMap
-import com.codahale.simplespec.{ignore, BeforeAndAfterEach, Spec}
+import com.codahale.wasp.Journal
+import com.codahale.simplespec.Spec
+import org.junit.Test
 
 class JournalSpec extends Spec {
-  class `Adding records to a journal` extends JournalContext {
-    def `reads them back when the journal is re-opened` = {
-      journal.add(1, "one")
-      journal.add(2, "two")
+  val tempDir = mkTempDir()
+  val _journal = newJournal()
 
-      journal.stop()
+  def journal = _journal
 
-      journal.start() must beEqualTo(SortedMap(1L -> "one", 2L -> "two"))
-    }
-  }
-
-  class `Adding records to a journal and then removing them` extends JournalContext {
-    def `doesn't read them back when the journal is re-opened` = {
-      journal.add(1, "one")
-      journal.add(2, "two")
-
-      journal.remove(2)
-      journal.remove(1)
-
-      journal.stop()
-
-      journal.start() must beEqualTo(SortedMap.empty[Long, String])
-    }
-  }
-
-  class `Adding records to a journal and then updating them` extends JournalContext {
-    def `reads the updated versions when the journal is re-opened` = {
-      journal.add(1, "one")
-      journal.add(2, "two")
-
-      journal.update(2, "TWO")
-      journal.update(1, "ONE")
-
-      journal.stop()
-
-      journal.start() must beEqualTo(SortedMap(1L -> "ONE", 2L -> "TWO"))
-    }
-  }
-}
-
-object JournalContext {
-  private val count = new AtomicInteger()
-}
-
-trait JournalContext extends BeforeAndAfterEach {
-
-  import JournalContext._
-
-  private val tempDir = mkTempDir()
-  private val _journal = newJournal()
-
-  @ignore
-  protected def journal = _journal
-
-  @ignore
-  protected def mkTempDir() = {
-    val dir = new File(System.getProperty("java.io.tmpdir"), "journal-" + count.incrementAndGet + "/")
+  def mkTempDir() = {
+    val dir = new File(System.getProperty("java.io.tmpdir"), "journal-" + System.nanoTime() + "/")
     dir.mkdirs()
     dir
   }
 
-  @ignore
-  protected def newJournal() =
+  def newJournal() =
     new Journal[String](tempDir, "temp.journal", StringCodec)
 
   override def beforeEach() {
@@ -88,5 +37,44 @@ trait JournalContext extends BeforeAndAfterEach {
       }
     }
     delete(tempDir)
+  }
+
+  class `Adding records to a journal` {
+    @Test def `reads them back when the journal is re-opened` = {
+      journal.add(1, "one")
+      journal.add(2, "two")
+
+      journal.stop()
+
+      journal.start().must(be(SortedMap(1L -> "one", 2L -> "two")))
+    }
+  }
+
+  class `Adding records to a journal and then removing them` {
+    @Test def `doesn't read them back when the journal is re-opened` = {
+      journal.add(1, "one")
+      journal.add(2, "two")
+
+      journal.remove(2)
+      journal.remove(1)
+
+      journal.stop()
+
+      journal.start().must(be(SortedMap.empty[Long, String]))
+    }
+  }
+
+  class `Adding records to a journal and then updating them` {
+    @Test def `reads the updated versions when the journal is re-opened` = {
+      journal.add(1, "one")
+      journal.add(2, "two")
+
+      journal.update(2, "TWO")
+      journal.update(1, "ONE")
+
+      journal.stop()
+
+      journal.start().must(be(SortedMap(1L -> "ONE", 2L -> "TWO")))
+    }
   }
 }
